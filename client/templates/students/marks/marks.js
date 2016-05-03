5658795282ffd0e4a3949539e59bff2625097848
m <mmailm@mail.ru>
2016-05-04 02:59:12 +0800

    implement table

--- client/templates/students/marks/marks.js
+++ client/templates/students/marks/marks.js
@@ -1,3 +1,23 @@
+let markClass = d3.scale.linear().domain([
+  -1001, -2, -1, 0, 1, 2, 3, 4, 5, 6, 1001
+]).range([
+  'black-hole', 'awful', 'bad', 'empty', 'normal', 'good', ' excellent', 'awesome', 'fantastic', 'incredible', 'shining'
+]);
+
+let lessonClass = d3.scale.linear().domain([
+  1, 2, 3, 4, 5
+]).range([
+  'laba', 'prct', 'test', 'lect', 'exam'
+]);
+
+function sortStudentsByName(a, b) {
+  return d3.ascending(a.second_name, b.second_name);
+}
+
+function sortStudentsByResult(a, b) {
+  return d3.ascending(a.second_name, b.second_name);
+}
+
 Template.marks.helpers({
   params() {
     return Router.current().route.params;
@@ -19,15 +39,89 @@ Template.marks.events({
 });
 
 Template.marks.onRendered(function () {
+  let tableStudents = d3.select("#students").append('table');
+  tableStudents.append("tr").attr("class", "mark-cell");
+  let tableMarks = d3.select("#marks").append('table');
+  let lessonsRow = tableMarks.append("tr");
+
   Tracker.autorun(function () {
     var marks = Router.current().marksInfo();
+    var lessons = marks.lessons;
+    var students = marks.students;
+    students = _.map(students, (v, i) => {
+      return v
+    });
+
     if (_.isEmpty(marks))
       return;
-    
-    let chart = d3.select("#barChart");
-    let svg = chart.append('svg');
-    // svg.selectAll("rect")
-    //   .data(marks)
-    console.log(marks)
+
+    function getXPos(i) {
+      return i * (itemSize.x + itemMargins.x);
+    }
+
+    function getYPos(i) {
+      return i * (itemSize.y + itemMargins.y);
+    }
+
+    // LESSONS LINE
+    let lessonsSelection = lessonsRow.selectAll("td").data(lessons);
+    lessonsSelection.enter().append("td").append("div");
+    lessonsSelection.select("div").attr({
+      class(l) {
+        return `mark-cell lesson ${lessonClass(l.lesson_type)}`
+      }
+    }).text((l) => {
+      return new Date(l.date).getUTCDate();
+    });
+    lessonsSelection.exit().remove();
+    //
+    // // MARK TABLE
+    let marksSelection = tableMarks.selectAll("tr.student").data(students);
+    marksSelection.enter().append("tr");
+    marksSelection.attr({
+      class: "student"
+    });
+    marksSelection.exit().remove();
+
+    let studentsMarksSelection = marksSelection.selectAll("td")
+      .data((s, i) => {
+        return s.marks
+      });
+    //
+    studentsMarksSelection.enter().append("td").append("div");
+    studentsMarksSelection.selectAll("div").data(d => {return [d]}).attr({
+      class(m) {
+        return `mark-cell mmark ${markClass(m.value)} ${lessonClass(m.lesson.lesson_type)}`
+      },
+      dataDate(m) {
+        return new Date(m.lesson.date).getUTCDate();
+      }
+    });
+    studentsMarksSelection.sort(sortStudentsByName);
+    studentsMarksSelection.exit().remove();
+
+    // STUDENTS TABLE
+    let studentSelection = tableStudents.selectAll("tr.student-row").data(students);
+    studentSelection.enter().append("tr").append("td").append("div");
+    studentSelection.attr({
+      class(s) {
+        return `student-row`;
+      }
+    }).sort(sortStudentsByName)
+      .selectAll("tr td div")
+      .data(d => {
+        return [d]
+      })
+      .attr({
+        class(s) {
+          return "student"
+        }
+      })
+      .text(s => {
+        return `${s.second_name} ${s.name}`
+      })
+
+
+    studentSelection.exit().remove();
   })
 });
